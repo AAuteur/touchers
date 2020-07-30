778c14085d09b8c3c0e94732516e45976b2b3164
AAuteur <aauteur@gmail.com>
2020-07-30 16:26:19 -0400

    fixed carousel and formatting

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ This website should pass the following Feature Requirements:
 - A navigation menu that collapses into a responsive mobile-friendly menu
 - Use CSS Grid to rearrange 2 or more content areas on your page as the screen changes
 - Use media queries to switch from a one-column mobile layout to a two or more column desktop layout
-- Create a form that takes in at least two fields of information and validates input
+- Create an image carousel with at least 3 images that it rotates through
 
-
-This project should also pass all of the Additional Requirements listed per the syllabus.
+This project should also satisfy all of the Additional Requirements listed per the syllabus.
--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,40 @@
-var slideIndex = 0; 
-showSlides();
-   
-function showSlides() { 
-    var i; 
-    
-    var slides = document.getElementsByClassName("image-sliderfade");  
-      
-    var dots = document.getElementsByClassName("dot");  
-  
-    for (i = 0; i < slides.length; i++) { 
-        slides[i].style.display = "none";  
-    } 
-   
-    slideIndex++;  
-   
-    if (slideIndex > slides.length) { 
-        slideIndex = 1; 
-    } 
-   
-    for (i = 0; i < dots.length; i++) { 
-        dots[i].className = dots[i].className. 
-                            replace(" active", ""); 
-    } 
-   
-    slides[slideIndex - 1].style.display = "block"; 
-    dots[slideIndex - 1].className += " active"; 
-  
-    setTimeout(showSlides, 2000);  
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
 }
+
+// var slideIndex = 0; 
+// showSlides();
+   
+// function showSlides() { 
+//     var i; 
+
+//     var slides = document.getElementsByClassName("image-sliderfade");  
+      
+//     var dots = document.getElementsByClassName("dot");  
+  
+//     for (i = 0; i < slides.length; i++) { 
+//         slides[i].style.display = "none";  
+//     } 
+   
+//     slideIndex++;  
+   
+//     if (slideIndex > slides.length) { 
+//         slideIndex = 1; 
+//     } 
+   
+//     for (i = 0; i < dots.length; i++) { 
+//         dots[i].className = dots[i].className. 
+//                             replace("active", ""); 
+//     } 
+   
+//     // slides[slideIndex - 1].style.display = "block"; 
+//     // dots[slideIndex - 1].className += "active";
+  
+//     setTimeout(showSlides, 2000);  
+// }
